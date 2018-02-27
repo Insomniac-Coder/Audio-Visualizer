fe9d312bfee4a285295f25206652594e91931819
Ishtdeep Malhi <ism1988@live.com>
2018-02-27 07:40:44 +0530

    initial commit

---
+++ Scripts/PostProcessingToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.UI;
+
+public class PostProcessingToggle : MonoBehaviour
+{
+    public Toggle checkMark;
+
+    public bool PostPorcessingEffects = true;
+    private void Update()
+    {
+        Check();
+        Toggle();
+    }
+    public void Check()
+    {
+        if (checkMark.isOn)
+        {
+            PostPorcessingEffects = true;
+        }
+        else
+        {
+            PostPorcessingEffects = false;
+        }
+    }
+    public void Toggle()
+    {
+        if (PostPorcessingEffects)
+        {
+            this.GetComponent<PostProcessingBehaviour>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<PostProcessingBehaviour>().enabled = false;
+        }
+    }
+}
